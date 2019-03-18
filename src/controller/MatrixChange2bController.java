059166b4c3a90f0d7dc4e67d64591cff6516e92d
w3ks0n <3bitxx1996!>
2019-03-18 15:08:53 +0100

    Sortowanie alfabetyczne klucza Obliczenie wymiarów tablicy dwuwymiarowej dla PlainText Utworzenie tablicy dwuwymiarowej przechowującej PlainText

--- src/controller/MatrixChange2bController.java
+++ src/controller/MatrixChange2bController.java
@@ -3,9 +3,10 @@ package controller;
 import javafx.scene.control.TextArea;
 import javafx.fxml.FXML;
 import java.lang.String;
+import java.util.Arrays;
 
 
-public class MatrixChange2bController{
+public class MatrixChange2bController {
 
     private MainController MainController;
 
@@ -19,39 +20,83 @@ public class MatrixChange2bController{
     private TextArea Cryptogram;
 
     @FXML
-    public void onPlainText(){
+    public void onPlainText() {
         Cryptogram.setText(ReplaceAllSpace(getPlainText()));
+        enterPlainTextIntoTheArray(createArray(rowsArray(), columnsArray()));
     }
 
     @FXML
-    public void onCryptogram(){
+    public void onCryptogram() {
+        Cryptogram.setText(sortKey(getKey()));
     }
 
     @FXML
-    public void onBack(){
+    public void onBack() {
         MainController.loadMenuScreen();
     }
 
     @FXML
-    public String getKey(){
+    public String getKey() {
         return Key.getText();
     }
 
     @FXML
-    public String getPlainText(){
+    public String getPlainText() {
         return PlainText.getText();
     }
 
     @FXML
-    public String getCryptogram(){
+    public String getCryptogram() {
         return Cryptogram.getText();
     }
 
-    public String ReplaceAllSpace(String CryptogramWithSpace){
-        return CryptogramWithSpace.replace(" ","");
+    public String ReplaceAllSpace(String CryptogramWithSpace) {
+        return CryptogramWithSpace.replace(" ", "");
+    }
+
+    public String sortKey(String unsortedKey) {
+        char tempArray[] = unsortedKey.toCharArray();
+        Arrays.sort(tempArray);
+        return new String(tempArray);
+    }
+
+    public int checkLengthPlainTextWithoutSpace(String plainTextWithoutSpace) {
+        return plainTextWithoutSpace.length();
+    }
+
+    public int checkLengthKey(String key) {
+        return key.length();
+    }
+
+    public int rowsArray() {
+        return new Integer((int) (Math.ceil(((double) (checkLengthPlainTextWithoutSpace(ReplaceAllSpace(getPlainText()))) / (double) (checkLengthKey(getKey()))))));
+    }
+
+    public int columnsArray() {
+        return new Integer(checkLengthKey(getKey()));
+    }
+
+    public String[][] createArray(int x, int y) {
+        return new String[rowsArray()][columnsArray()];
     }
 
     public void setMainController(MainController mainController) {
         MainController = mainController;
+    }
+
+    public void enterPlainTextIntoTheArray(String array[][]) {
+        int x = 0;
+        for (int i = 0; i < rowsArray(); i++) {
+            for (int j = 0; j < columnsArray(); j++, x++) {
+                //IF tylko dla przypadku PlainText=24, Key=7
+                if (i == 3 && j == 3) {
+                    break;
+                } else {
+                    array[i][j] = String.valueOf(((ReplaceAllSpace(getPlainText())).charAt(x)));
+                    System.out.println(array[i][j]);
+                }
+            }
+
+        }
     }
 }
