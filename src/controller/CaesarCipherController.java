a038d130b47dac43659e0fa3a181e2d4193312ea
w3ks0n <3bitxx1996!>
2019-03-26 23:05:48 +0100

    Logika do przestawień macierzowych Poprawiona wersja kodu Szyfru Cezara

--- src/controller/CaesarCipherController.java
+++ src/controller/CaesarCipherController.java
@@ -1,9 +1,13 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
 
+
 public class CaesarCipherController {
+
+    private MainController MainController;
 
     private static int firstLetterInASCII = 65;
     private static int lastLetterInASCII = 90;
@@ -66,18 +70,30 @@ public class CaesarCipherController {
     }
 
     public String encryptPlainText(char[] plainText) {
-        int numberOfLitersToTheEndOfTheAlphabet, numberOfPassesFromTheBeginningOfTheAlphabet, ASCIIletter, key = convertStringToInt(getKey()), lengthPlainText = checkLengthWord(plainText);
+        int numberOfLitersToTheEndOfTheAlphabet;
+        int numberOfPassesFromTheBeginningOfTheAlphabet;
+        int currentLetter;
+        int key=0;
+        int lengthPlainText = checkLengthWord(plainText);
         String encryptedMessage = new String();
+        try{
+            key = convertStringToInt(getKey());
+        }catch (Exception ex){
+            Alert dg = new Alert(Alert.AlertType.INFORMATION);
+            dg.setTitle("Error");
+            dg.setHeaderText("Enter the value of the key\n");
+            dg.show();
+        }
+
         for (int i = 0; i < lengthPlainText; i++) {
-            ASCIIletter = (int) plainText[i];
-            if (ASCIIletter + key <= lastLetterInASCII) {
-                plainText[i] = (char) ((ASCIIletter) + key);
+            currentLetter = (int) plainText[i];
+            if (currentLetter + key <= lastLetterInASCII) {
+                plainText[i] = (char) ((currentLetter) + key);
             } else {
-                numberOfLitersToTheEndOfTheAlphabet = lastLetterInASCII - ASCIIletter;
+                numberOfLitersToTheEndOfTheAlphabet = lastLetterInASCII - currentLetter;
                 numberOfPassesFromTheBeginningOfTheAlphabet = key - numberOfLitersToTheEndOfTheAlphabet - 1;
                 plainText[i] = (char) (firstLetterInASCII + numberOfPassesFromTheBeginningOfTheAlphabet);
             }
-            System.out.print(plainText[i]);
         }
         for (int i = 0; i < lengthPlainText; i++) {
             encryptedMessage = encryptedMessage + Character.toString(plainText[i]);
@@ -86,28 +102,38 @@ public class CaesarCipherController {
     }
 
     public String descriptionCryptogram(char[] cryptogram){
-        int lengthCryptogram = checkLengthWord(cryptogram), key=convertStringToInt(getKey()), ASCIIletter, numberOfLitersToTheBeginningOfTheAlphabet, numberOfPassesFromTheEndOfTheAlphabet;
+        int lengthCryptogram = checkLengthWord(cryptogram);
+        int key=0;
+        int currentLetter;
+        int numberOfLitersToTheBeginningOfTheAlphabet;
+        int numberOfPassesFromTheEndOfTheAlphabet;
         String decryptedMessage = new String();
+        try{
+            key = convertStringToInt(getKey());
+        }catch (Exception ex){
+            Alert dg = new Alert(Alert.AlertType.INFORMATION);
+            dg.setTitle("Error");
+            dg.setHeaderText("Enter the value of the key\n");
+            dg.show();
+        }
+
         for(int i=0; i<lengthCryptogram; i++){
-            ASCIIletter = cryptogram[i];
-            if(ASCIIletter - key >= firstLetterInASCII){
-                cryptogram[i] = (char)((ASCIIletter-key));
+            currentLetter = cryptogram[i];
+            if(currentLetter - key >= firstLetterInASCII){
+                cryptogram[i] = (char)((currentLetter-key));
             }
             else{
-                numberOfLitersToTheBeginningOfTheAlphabet = ASCIIletter - firstLetterInASCII;
+                numberOfLitersToTheBeginningOfTheAlphabet = currentLetter - firstLetterInASCII;
                 numberOfPassesFromTheEndOfTheAlphabet = key - numberOfLitersToTheBeginningOfTheAlphabet-1;
                 cryptogram[i]=(char)(lastLetterInASCII-numberOfPassesFromTheEndOfTheAlphabet);
 
             }
-            //System.out.print(cryptogram[i]);
         }
         for(int i=0; i<lengthCryptogram; i++){
             decryptedMessage = decryptedMessage + Character.toString(cryptogram[i]);
         }
             return decryptedMessage;
     }
-
-    private MainController MainController;
 
     public void setMainController(controller.MainController mainController) {
         MainController = mainController;
--- src/controller/MatrixChange2bController.java
+++ src/controller/MatrixChange2bController.java
@@ -2,9 +2,8 @@ package controller;
 
 import javafx.scene.control.TextArea;
 import javafx.fxml.FXML;
-import java.lang.String;
-import java.util.Arrays;
 
+import java.lang.String;
 
 public class MatrixChange2bController {
 
@@ -21,13 +20,20 @@ public class MatrixChange2bController {
 
     @FXML
     public void onPlainText() {
-        Cryptogram.setText(ReplaceAllSpace(getPlainText()));
-        enterPlainTextIntoTheArray(createArray(rowsArray(), columnsArray()));
+        int LengthPlainText = checkPlanTextLength(stringPlainTextConvertToChar(ReplaceAllSpace(getPlainText())));
+        int LengthKey = checkCharKeyLength(stringKeyConvertToChar(getKey()));
+        char[][] encrypt = encrypt(LengthKey, LengthPlainText);
+        char[][] Sort = sortCharKey(encrypt,LengthKey,LengthPlainText);
+        String encryptFinalVersion = encryptCombine(Sort,LengthKey, LengthPlainText);
+        Cryptogram.setText(encryptFinalVersion);
     }
 
     @FXML
     public void onCryptogram() {
-        Cryptogram.setText(sortKey(getKey()));
+        int charCryptogramLength = checkPlanTextLength(stringPlainTextConvertToChar(getCryptogram()));
+        int LengthKey = checkCharKeyLength(stringKeyConvertToChar(getKey()));
+        String decryptionFinalVersion = decryption(LengthKey,charCryptogramLength);
+        PlainText.setText(decryptionFinalVersion);
     }
 
     @FXML
@@ -54,49 +60,168 @@ public class MatrixChange2bController {
         return CryptogramWithSpace.replace(" ", "");
     }
 
-    public String sortKey(String unsortedKey) {
-        char tempArray[] = unsortedKey.toCharArray();
-        Arrays.sort(tempArray);
-        return new String(tempArray);
-    }
-
-    public int checkLengthPlainTextWithoutSpace(String plainTextWithoutSpace) {
-        return plainTextWithoutSpace.length();
-    }
-
-    public int checkLengthKey(String key) {
-        return key.length();
-    }
-
-    public int rowsArray() {
-        return new Integer((int) (Math.ceil(((double) (checkLengthPlainTextWithoutSpace(ReplaceAllSpace(getPlainText()))) / (double) (checkLengthKey(getKey()))))));
-    }
-
-    public int columnsArray() {
-        return new Integer(checkLengthKey(getKey()));
-    }
-
-    public String[][] createArray(int x, int y) {
-        return new String[rowsArray()][columnsArray()];
+    public char[] stringPlainTextConvertToChar(String stringPlainText) {
+        return stringPlainText.toCharArray();
     }
 
     public void setMainController(MainController mainController) {
         MainController = mainController;
     }
 
-    public void enterPlainTextIntoTheArray(String array[][]) {
-        int x = 0;
-        for (int i = 0; i < rowsArray(); i++) {
-            for (int j = 0; j < columnsArray(); j++, x++) {
-                //IF tylko dla przypadku PlainText=24, Key=7
-                if (i == 3 && j == 3) {
+    public char[] stringKeyConvertToChar(String stringKey) {
+        return stringKey.toCharArray();
+    }
+
+    public int checkCharKeyLength(char[] key) {
+        return key.length;
+    }
+
+    public int checkPlanTextLength(char[] plainText) {
+        return plainText.length;
+    }
+
+    public char[][] sortCharKey(char[][] unsortedArray, int charKeyLength, int charPlainTextLength) {
+        int numberOfRows = ((int)(Math.ceil(((double)(charPlainTextLength))/((double)(charKeyLength)))+2.0));
+        char[] ArrayTemp = new char[charKeyLength];
+        for (int i = 1; i < charKeyLength; i++) {
+            for (int j = 0; j < charKeyLength-1; j++) {
+                if (unsortedArray[1][j] > unsortedArray[1][j+1]) {
+                    for(int k=0; k<numberOfRows; k++){
+                            ArrayTemp[k] = unsortedArray[k][j];
+                            unsortedArray[k][j]=unsortedArray[k][j+1];
+                            unsortedArray[k][j+1]=ArrayTemp[k];
+                    }
+                }
+            }
+        }
+        return unsortedArray;
+    }
+
+    public char[][] encrypt(int charKeyLength, int charPlainTextLength) {
+        int numberOfRows = ((int)(Math.ceil(((double)(charPlainTextLength))/((double)(charKeyLength)))+2.0));
+        int currentLetter = 0;
+        char[][] Array2 = new char[numberOfRows][charKeyLength];
+        for (int j = 0; j < charKeyLength; j++) {
+            Array2[0][j] = (char) j;
+        }
+        for (int j = 0; j < charKeyLength; j++) {
+            Array2[1][j] = stringKeyConvertToChar(getKey())[j];
+        }
+        for (int i = 2; i < numberOfRows; i++) {
+            for (int j = 0; j < charKeyLength; j++, currentLetter++) {
+                if ((i == numberOfRows-1) && j==((checkPlanTextLength(stringPlainTextConvertToChar(ReplaceAllSpace(getPlainText()))))%(charKeyLength))) {
                     break;
-                } else {
-                    array[i][j] = String.valueOf(((ReplaceAllSpace(getPlainText())).charAt(x)));
-                    System.out.println(array[i][j]);
+                }
+                else{
+                    Array2[i][j] = (stringPlainTextConvertToChar(ReplaceAllSpace(getPlainText())))[currentLetter];
+                }
+            }
+        }
+        int lastRow=numberOfRows-1;
+        int theLastNonEmptyIndexInTheLastRow=((checkPlanTextLength(stringPlainTextConvertToChar(ReplaceAllSpace(getPlainText()))))%(charKeyLength));
+
+           for (int j = theLastNonEmptyIndexInTheLastRow; j < charKeyLength; j++) {
+                if ((j>=theLastNonEmptyIndexInTheLastRow)) {
+                    Array2[lastRow][j]='0';
+                }
+                else{
+                    continue;
+                }
+            }
+        return Array2;
+    }
+
+    public String encryptCombine(char[][] sortArray, int charKeyLength, int charPlainTextLength ){
+
+        int numberOfRows = ((int)(Math.ceil(((double)(charPlainTextLength))/((double)(charKeyLength)))+2.0));
+
+        String encryptedMessage = new String();
+        for(int i=0; i<charKeyLength; i++){
+            for(int j=2; j<numberOfRows; j++){
+                encryptedMessage = encryptedMessage + Character.toString(sortArray[j][i]);
+            }
+        }
+        encryptedMessage = encryptedMessage.replace("0", "");
+        return encryptedMessage;
+    }
+
+    public String decryption(int charKeyLength, int charCryptogramLength){
+        int fullColumns = ((int)(Math.ceil(((double)(charCryptogramLength))/((double)(charKeyLength)))+2.0));
+        char[][] Array2 = new char[fullColumns][charKeyLength];
+        int n = charCryptogramLength%charKeyLength;
+        char[] arrayTemp = new char[charKeyLength-(n)];
+        int incompleteColumn=0;
+        int indexIncompleteColumn=0;
+        int currentLetter=0;
+        int P=0;
+        String decryptedMessage = new String();
+        for (int j = 0; j < charKeyLength; j++) {
+            Array2[0][j] = (char) j;
+        }
+        for (int j = 0; j < charKeyLength; j++) {
+            Array2[1][j] = stringKeyConvertToChar(getKey())[j];
+        }
+        for(int k=0; k<charKeyLength-(n); k++){
+            arrayTemp[k]=Array2[0][n+k];
+        }
+        char[] temp = new char[charKeyLength];
+        for(int i=0; i<charKeyLength; i++){
+            for(int j=0; j<charKeyLength-1; j++){
+                if(Array2[1][j]>Array2[1][j+1]){
+                    for(int k=0; k<2; k++){
+                        temp[k] = Array2[k][j];
+                        Array2[k][j]=Array2[k][j+1];
+                        Array2[k][j+1]=temp[k];
+                    }
+                }
+            }
+        }
+        while(incompleteColumn<arrayTemp.length){
+            if(Array2[0][indexIncompleteColumn]==arrayTemp[incompleteColumn]){
+                arrayTemp[incompleteColumn]=(char)indexIncompleteColumn;
+                incompleteColumn++;
+                indexIncompleteColumn=0;
+            }
+            else{
+                indexIncompleteColumn=indexIncompleteColumn+1;
+            }
+
+        }
+        for (int i = 0; i < charKeyLength; i++) {
+            for (int j = 2; j < fullColumns; j++) {
+                P=0;
+                for(int k=0; k<charKeyLength-(n); k++){
+                    if(j==fullColumns-1 && (i==arrayTemp[k])){
+                        Array2[j][i]='0';
+                        P=1;
+                        break;
+                    }
+                }
+                if(P==0){
+                    Array2[j][i] = (stringPlainTextConvertToChar(getCryptogram()))[currentLetter];
+                    currentLetter++;
                 }
             }
 
         }
-    }
-}
+        for(int i=0; i<charKeyLength; i++){
+            for(int j=0; j<charKeyLength-1; j++){
+                if(Array2[0][j]>Array2[0][j+1]){
+                    for(int k=0; k<fullColumns; k++){
+                        temp[k] = Array2[k][j];
+                        Array2[k][j]=Array2[k][j+1];
+                        Array2[k][j+1]=temp[k];
+                    }
+                }
+            }
+        }
+        for(int i=2; i<fullColumns; i++){
+            for(int j=0; j<charKeyLength; j++){
+                String s = Character.toString(Array2[i][j]);
+                decryptedMessage = decryptedMessage + s;
+            }
+        }
+        decryptedMessage = decryptedMessage.replace("0", "");
+        return  decryptedMessage;
+            }
+        }
